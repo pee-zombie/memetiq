180f960b883cef0730b25a19d61443a81b5fee07
pee-zombie <pee_zombie@protomail.com>
2021-08-21 21:49:13 -0400

    replace scala with java spring boot

---
+++ app/src/main/java/systems/memetic/memetiq/MemetiqApplication.java
@@ -0,0 +1,13 @@
+package systems.memetic.memetiq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemetiqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemetiqApplication.class, args);
+    }
+
+}
