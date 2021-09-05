4c68f34f298cf1394bd6bb2c84924604bbb7cf0d
pee-zombie <pee_zombie@protomail.com>
2021-09-05 01:06:48 -0400

    configure jooq code gen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,11 @@
+import nu.studer.gradle.jooq.JooqEdition
+import org.jooq.meta.jaxb.Property
+
 plugins {
     id("org.springframework.boot") version "2.5.4"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
+
+    id("nu.studer.jooq") version "6.0.1"
 
     application
 }
@@ -17,6 +22,81 @@ configurations {
     compileOnly {
         extendsFrom(configurations.annotationProcessor.get())
     }
+}
+
+jooq {
+//    version.set(dependencyManagement.importedProperties["jooq.version"])
+    edition.set(JooqEdition.OSS)  // the default (can be omitted)
+
+    configurations {
+        create("main") {
+            jooqConfiguration.apply {
+                generator.apply {
+                    database.apply {
+                        name = "org.jooq.meta.extensions.ddl.DDLDatabase"
+
+                        properties.addAll(
+                            arrayOf(
+                                // Specify the location of your SQL script.
+                                // You may use ant-style file matching, e.g. /path/**/to/*.sql
+                                //
+                                // Where:
+                                // - ** matches any directory subtree
+                                // - * matches any number of characters in a directory / file name
+                                // - ? matches a single character in a directory / file name
+                                Property()
+                                    .withKey("scripts")
+                                    .withValue("src/main/resources/data.sql")
+
+                                // The sort order of the scripts within a directory, where:
+                                //
+                                // - semantic: sorts versions, e.g. v-3.10.0 is after v-3.9.0 (default)
+                                // - alphanumeric: sorts strings, e.g. v-3.10.0 is before v-3.9.0
+                                // - flyway: sorts files the same way as flyway does
+                                // - none: doesn"t sort directory contents after fetching them from the directory
+//                    property {
+//                        key = "sort"
+//                        value = "semantic"
+//                    }
+
+                                // The default schema for unqualified objects:
+                                //
+                                // - public: all unqualified objects are located in the PUBLIC (upper case) schema
+                                // - none: all unqualified objects are located in the default schema (default)
+                                //
+                                // This configuration can be overridden with the schema mapping feature
+//                    property {
+//                        key = "unqualifiedSchema"
+//                        value = "none"
+//                    }
+
+                                // The default name case for unquoted objects:
+                                //
+                                // - as_is: unquoted object names are kept unquoted
+                                // - upper: unquoted object names are turned into upper case (most databases)
+                                // - lower: unquoted object names are turned into lower case (e.g. PostgreSQL)
+//                    property {
+//                        key = "defaultNameCase"
+//                        value = "as_is"
+//                    }
+                            )
+                        )
+                    }
+
+                    target.apply {
+                        packageName = "systems.memetic.memetiq"
+                        directory = "src/generated/jooq/"
+                    }
+                    strategy.name = "org.jooq.codegen.DefaultGeneratorStrategy"
+
+                }
+            }
+        }
+    }
+}
+
+tasks.named<nu.studer.gradle.jooq.JooqGenerate>("generateJooq") {
+    allInputsDeclared.set(true)
 }
 
 repositories {
@@ -43,6 +123,9 @@ dependencies {
 
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
+
+    jooqGenerator("org.jooq:jooq-meta-extensions:3.15.2")
+    jooqGenerator("com.h2database:h2:1.4.200")
 
     developmentOnly("org.springframework.boot:spring-boot-devtools")
 
